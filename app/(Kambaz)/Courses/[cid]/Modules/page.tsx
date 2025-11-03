0d5548d020f077afe08584a13625deafec5c3b7e
Aryan Shah <aryanshah295@gmail.com>
2025-11-03 18:22:45 -0500

    minor fix

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,37 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import { addModule, editModule, updateModule, deleteModule }
-  from "./reducer";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../../store";
-import { useState } from "react";
-import { Container, ListGroup } from "react-bootstrap";
+
+import { Container, FormControl, ListGroup } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
-import { FormControl } from "react-bootstrap";
-import { useParams } from "next/navigation";
 
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 export default function Modules() {
   const { cid } = useParams();
   const [moduleName, setModuleName] = useState("");
-  const { modules } = useSelector((state: RootState) => state.modulesReducer);
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const studentView = currentUser.role === "STUDENT";
   const dispatch = useDispatch();
-
   return (
-    <div>
-      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={() => {
-          dispatch(addModule({ name: moduleName, course: cid }));
-          setModuleName("");
-      }} />
-      <br />
-      <br />
-      <br />
-      <br />
+    <Container>
+      {!studentView && (
+        <>
+          <ModulesControls
+            moduleName={moduleName}
+            setModuleName={setModuleName}
+            addModule={() => {
+              dispatch(addModule({ name: moduleName, course: cid }));
+              setModuleName("");
+            }}
+          />
+          <br />
+          <br />
+          <br />
+          <br />
+        </>
+      )}
       <ListGroup className="rounded-0" id="wd-modules">
         {modules
-          .filter((module) => module.course === cid)
-          .map((module) => (
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
             <ListGroup.Item
               key={module._id}
               className="wd-module p-0 mb-5 fs-5 border-gray"
@@ -39,30 +47,40 @@ export default function Modules() {
               <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
                 {!module.editing && module.name}
-                { module.editing && (
-                  <FormControl className="w-50 d-inline-block"
-                        onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") {
-                            dispatch(updateModule({ ...module, editing: false }));
-                          }
-                        }}
-                        defaultValue={module.name}/>
+                {module.editing && (
+                  <FormControl
+                    className="w-50 d-inline-block"
+                    onChange={(e) =>
+                      dispatch(
+                        updateModule({ ...module, name: e.target.value })
+                      )
+                    }
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        dispatch(updateModule({ ...module, editing: false }));
+                      }
+                    }}
+                    defaultValue={module.name}
+                  />
                 )}
-                <ModuleControlButtons moduleId={module._id}
-                  deleteModule={(moduleId) => {
-                    dispatch(deleteModule(moduleId));
-                  }}
-                  editModule={(moduleId) => dispatch(editModule(moduleId))} />
+                {!studentView && (
+                  <ModuleControlButtons
+                    moduleId={module._id}
+                    deleteModule={(moduleId) => {
+                      dispatch(deleteModule(moduleId));
+                    }}
+                    editModule={(moduleId) => dispatch(editModule(moduleId))}
+                  />
+                )}
               </div>
               {module.lessons && (
                 <ListGroup className="wd-lessons rounded-0">
-                  {module.lessons.map((lesson) => (
+                  {module.lessons.map((lesson: any) => (
                     <ListGroup.Item
                       key={lesson._id}
                       className="wd-lesson p-3 ps-1"
                     >
-                      <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                      <BsGripVertical className="me-2 fs-3" /> {lesson.name}
                       <LessonControlButtons />
                     </ListGroup.Item>
                   ))}
@@ -71,6 +89,6 @@ export default function Modules() {
             </ListGroup.Item>
           ))}
       </ListGroup>
-    </div>
+    </Container>
   );
 }
